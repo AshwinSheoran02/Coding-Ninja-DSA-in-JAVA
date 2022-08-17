7f99d2bee8debc37f5b7e2cb620560d8cd7aff27
Ashwin Sheoran <88393756+AshwinSheoran02@users.noreply.github.com>
2022-08-17 18:00:56 +0530

    Recursion

---
+++ 2.Data_Structures_in_JAVA/L3-Recursion-1/All_Indices_of_Number.java
@@ -0,0 +1,43 @@
+// Given an array of length N and an integer x, you need to find all the indexes where x is present in the input array. Save all the indexes in an array (in increasing order).
+// Do this recursively. Indexing in the array starts from 0.
+// Input Format :
+// Line 1 : An Integer N i.e. size of array
+// Line 2 : N integers which are elements of the array, separated by spaces
+// Line 3 : Integer x
+// Output Format :
+// indexes where x is present in the array (separated by space)
+
+
+public class Solution {
+
+	public static int[] allIndexes(int input[], int x) {
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+	 	 * Taking input and printing output is handled automatically.
+		*/
+        
+        return allIndices(input, x, 0);
+		
+	}
+    
+    private static int[] allIndices(int[] arr, int x, int startIndex) {
+        if (startIndex >= arr.length) {
+            int[] output = new int[0];
+            return output;
+        }
+        int smallOutput[] = allIndices(arr, x, startIndex + 1);
+        if (arr[startIndex] == x) {
+            int output[] = new int[smallOutput.length + 1];
+            output[0] = startIndex;
+            for (int i = 0; i < smallOutput.length; i++) {
+                output[i + 1] = smallOutput[i];
+            }
+            return output;
+        } else {
+            return smallOutput;
+        }
+    }
+	
+}
