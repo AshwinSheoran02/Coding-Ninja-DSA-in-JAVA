006672ad1ac2d3a6b834ac6088a08b3a57455d8e
Ashwin Sheoran <88393756+AshwinSheoran02@users.noreply.github.com>
2022-08-19 21:18:30 +0530

    TandS2

---
+++ 2.Data_Structures_in_JAVA/L6-Time_and_Space_Complexity/Duplicate_in_Array.java
@@ -0,0 +1,43 @@
+// You have been given an integer array/list(ARR) of size N. Where N is equal to [2M + 1].
+// Now, in the given array/list, 'M' numbers are present twice and one number is present only once.
+// You need to find and return that number which is unique in the array/list.
+//  Note:
+// Unique element is always present in the array/list according to the given condition.
+// Input format :
+// The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+// First line of each test case or query contains an integer 'N' representing the size of the array/list.
+
+// Second line contains 'N' single space separated integers representing the elements in the array/list.
+// Output Format :
+// For each test case, print the unique element present in the array.
+
+import java.util.*;
+public class Solution {
+
+	public static int findUnique(int[] arr) {
+		//Your code goes here
+          int XOR = 0;;
+        for(int i=0;i<arr.length;i++)
+        {
+           XOR ^= arr[i];    
+        }
+        return XOR;  //time complexity is O(n) ans space Complexity is O(1).
+	}
+    public static int findUnique1(int[] arr) {
+		//Your code goes here
+        if(arr.length == 0){
+            return -1;
+        }
+        Arrays.sort(arr); //nlogn
+        if(arr[0]!=arr[1]){
+            return arr[0]; //for 1st unique element
+        }
+        for(int i=1;i<arr.length-1;i++){  //bigo of n.
+            if(arr[i]!=arr[i+1] && arr[i]!=arr[i-1]){
+                return arr[i];
+            }
+        }
+        return -1; //final time complexity = nlogn+n
+    }
+}
